e755f740b294d2e9ea2e84814f65f097406b25a0
Ajay Kumar <aks969014@gmail.com>
2024-11-11 23:53:02 +0530

    done number

---
+++ number/PaskalTriangle.java
@@ -0,0 +1,18 @@
+package number;
+
+public class PaskalTriangle {
+    public static void main(String[] args) {
+        int n = 5; // Number of rows
+        for (int i = 0; i < n; i++) {
+            for (int j = n - i; j > 1; j--) {
+                System.out.print(" ");
+            }
+            int number = 1;
+            for (int k = 0; k <= i; k++) {
+                System.out.print(number + " ");
+                number = number * (i - k) / (k + 1);
+            }
+            System.out.println();
+        }
+    }
+}

5d04d21c80fd6a60dee648616fcfb533892846e7
Ajay Kumar <aks969014@gmail.com>
2024-11-27 22:49:08 +0530

    done hollow stars pattern

---
+++ randomQuestion/diamondPattern.java
@@ -0,0 +1,30 @@
+// 
+//         *
+//        ***
+//       *****
+//        ***
+//         *
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+
+
+
+
+package randomQuestion;
+
+public class diamondPattern {
+    public static void main(String[] args) {
+        int n= 5;
+        for(int i = 1; i <= n; i++){
+            
+        }
+    }
+    
+}

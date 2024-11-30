1e14bbedec1215b5a0b3b29efe3f354958093869
Ajay Kumar <aks969014@gmail.com>
2024-11-30 20:52:10 +0530

    done

---
+++ randomQuestion/hollowstar.java
@@ -0,0 +1,24 @@
+//                  *
+//                *    *
+//              *        *
+//            *            *
+//              *        *
+//                *    *
+//                  *
+// 
+//  
+//  
+//  
+ package randomQuestion;
+
+public class hollowstar {
+    // Function to print hollow star pattern
+    public static void main(String[] args) {
+        int n = 5;
+        int outerSpace  = 
+
+        for (int i = 1; i <= n; i++){
+
+        }
+    }
+}

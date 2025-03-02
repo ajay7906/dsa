a4a278bf447d55688719438eb965dd551bf4a7d5
Ajay Kumar <aks969014@gmail.com>
2025-03-02 23:20:34 +0530

    done

--- js/linkedlist/linked.js
+++ js/linkedlist/linked.js
@@ -1,8 +1,18 @@
+// Node class represents each element in the linked list
 class Node {
-    constructor(value){
-        this.value = value;
-        this.next = null;
-
+    constructor(data) {
+      this.data = data;  // Data of the node
+      this.next = null;   // Points to the next node (null initially)
     }
-}
-//linked list to manage the list
+  }
+  
+  // LinkedList class manages the linked list operations
+  class LinkedList {
+    constructor() {
+      this.head = null;  // Points to the first node in the list
+    }
+  }
+  
+  // Example usage: Create an empty linked list
+  const list = new LinkedList();
+  

48c2f6aebc19eccac4acc07f6a1da1c20b954b68
Ajay Kumar <aks969014@gmail.com>
2025-04-12 14:47:09 +0530

    done

--- arr/Obj.js
+++ arr/Obj.js
@@ -72,32 +72,84 @@
 
 
 
-  const nested = {
-    a: {
-      b: {
-        c: 1
-      }
-    },
-    d: 2
-  };
+  // const nested = {
+  //   a: {
+  //     b: {
+  //       c: 1
+  //     }
+  //   },
+  //   d: 2
+  // };
   
-  function flattenObject(obj) {
-    const result = {};
+  // function flattenObject(obj) {
+  //   const result = {};
     
-    for (let key in obj) {
-      if (typeof obj[key] === 'object' && obj[key] !== null && !Array.isArray(obj[key])) {
-        const flatObject = flattenObject(obj[key]);
-        for (let innerKey in flatObject) {
-          result[`${key}.${innerKey}`] = flatObject[innerKey];
-        }
-      } else {
-        result[key] = obj[key];
-      }
+  //   for (let key in obj) {
+  //     if (typeof obj[key] === 'object' && obj[key] !== null && !Array.isArray(obj[key])) {
+  //       const flatObject = flattenObject(obj[key]);
+  //       for (let innerKey in flatObject) {
+  //         result[`${key}.${innerKey}`] = flatObject[innerKey];
+  //       }
+  //     } else {
+  //       result[key] = obj[key];
+  //     }
+  //   }
+  
+  //   return result; // ✅ missing in your version
+  // }
+  
+  // const flat = flattenObject(nested);
+  // console.log(flat);
+  
+
+  //reverse a string
+  let a = 'ajay';
+  function reverseString(str){
+    let reversed = '';
+    for (let i = str.length - 1; i>= 0; i--){
+      reversed = reversed + str[i];
+
     }
-  
-    return result; // ✅ missing in your version
+    return reversed;
   }
-  
-  const flat = flattenObject(nested);
-  console.log(flat);
-  
+console.log(
+  reverseString(a));
+
+  //solver without new strin
+  function revereWithOutNewString(str){
+    let arr = str.split('');
+    console.log(arr);
+    let left = 0;
+    let right = arr.length - 1;
+    while (left < right){
+      [arr[left], arr[right]] = [arr[right], arr[left]];
+      left++;
+      right--;
+    }
+    console.log(arr);
+    return arr.join('');
+    
+  }
+console.log(
+  revereWithOutNewString(a));
+
+
+  //check palindrome
+  function checkPalidRome(){
+    let str = 'madam1';
+    let left = 0;
+    let right = str.length - 1;
+    while(left < right){
+      if (str[left] !== str[right]){
+        return 'this is not palidrome';
+      }
+      left++;
+      right--;
+    }
+    return 'this is palidrome';
+  }
+
+
+  console.log(
+    checkPalidRome()
+  );

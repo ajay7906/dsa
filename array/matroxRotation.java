0e78d9b804781e0b2ec28c415c647fed031d585f
Ajay Kumar <aks969014@gmail.com>
2026-05-30 20:06:30 +0530

    submit the spiral matrix

--- array/matroxRotation.java
+++ array/matroxRotation.java
@@ -13,16 +13,28 @@ public class matroxRotation {
         // Transpose the matrix
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
+
                 int temp = matrix[i][j];
+                // System.out.println("temp: " + temp);
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
+        }
+
+        // print the tranposed matrix
+        System.out.println("Transposed Matrix:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
         }
 
         // Reverse each row
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n / 2; j++) {
                 int temp = matrix[i][j];
+                System.out.println("temp: " + temp);
                 matrix[i][j] = matrix[i][n - 1 - j];
                 matrix[i][n - 1 - j] = temp;
             }
